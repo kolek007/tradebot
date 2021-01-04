4d5fafcf33b9c787412f62bcf6487887ac9e0ca5
nlyubomishchenko <nlyubomishchenko@axiomsl.com>
2021-01-05 00:39:00 +0300

    added trading parameters class

--- README.md
+++ README.md
@@ -8,6 +8,7 @@
 - Create folder "tradebot"
 - Navigate to created folder and open Git Bash
 - Run command "git clone https://github.com/kolek007/tradebot.git"
-- Run command "git clone https://github.com/TinkoffCreditSystems/invest-openapi-java-sdk.git"
 - Open IDEA, create new project from existing sources (choose tradebot/build.gradle file)
-- Press File->New->Module From Existing Sources...-> Choose tradebot\invest-openapi-java-sdk\pom.xml 
+
+## Tinkoff API   
+Learn about tinkoff api here https://github.com/TinkoffCreditSystems/invest-openapi-java-sdk.git
--- src/main/java/org/nl/TradingParameters.java
+++ src/main/java/org/nl/TradingParameters.java
@@ -1,59 +1,61 @@
-//package org.nl;
-//
-//
-//import javax.annotation.Nonnull;
-//import java.util.Arrays;
-//
-//public class TradingParameters {
-//    @Nonnull
-//    public final String ssoToken;
-//    @Nonnull
-//    public final String[] tickers;
-//    @Nonnull
-//    public final CandleInterval[] candleIntervals;
-//    public final boolean sandboxMode;
-//
-//    public TradingParameters(@Nonnull final String ssoToken,
-//                             @Nonnull final String[] tickers,
-//                             @Nonnull final CandleInterval[] candleIntervals,
-//                             final boolean sandboxMode) {
-//        this.ssoToken = ssoToken;
-//        this.tickers = tickers;
-//        this.candleIntervals = candleIntervals;
-//        this.sandboxMode = sandboxMode;
-//    }
-//
-//    @Nonnull
-//    public static TradingParameters fromProgramArgs(@Nonnull final String ssoTokenArg,
-//                                                    @Nonnull final String tickersArg,
-//                                                    @Nonnull final String candleIntervalsArg,
-//                                                    @Nonnull final String sandboxModeArg) {
-//        final String[] tickers = tickersArg.split(",");
-//        final CandleInterval[] candleIntervals = Arrays.stream(candleIntervalsArg.split(","))
-//                .map(TradingParameters::parseCandleInterval)
-//                .toArray(value -> new CandleInterval[0]);
-//        if (candleIntervals.length != tickers.length)
-//            throw new IllegalArgumentException("Количество переданных разрешающих интервалов свечей не совпадает с переданным количеством тикеров.");
-//
-//        final boolean useSandbox = Boolean.parseBoolean(sandboxModeArg);
-//
-//        return new TradingParameters(ssoTokenArg, tickers, candleIntervals, useSandbox);
-//    }
-//
-//    private static CandleInterval parseCandleInterval(final String str) {
-//        switch (str) {
-//            case "1min":
-//                return CandleInterval.ONE_MIN;
-//            case "2min":
-//                return CandleInterval.TWO_MIN;
-//            case "3min":
-//                return CandleInterval.THREE_MIN;
-//            case "5min":
-//                return CandleInterval.FIVE_MIN;
-//            case "10min":
-//                return CandleInterval.TEN_MIN;
-//            default:
-//                throw new IllegalArgumentException("Не распознан разрешающий интервал!");
-//        }
-//    }
-//}
+package org.nl;
+
+
+import ru.tinkoff.invest.openapi.models.market.CandleInterval;
+
+import javax.annotation.Nonnull;
+import java.util.Arrays;
+
+public class TradingParameters {
+    @Nonnull
+    public final String ssoToken;
+    @Nonnull
+    public final String[] tickers;
+    @Nonnull
+    public final CandleInterval[] candleIntervals;
+    public final boolean sandboxMode;
+
+    public TradingParameters(@Nonnull final String ssoToken,
+                             @Nonnull final String[] tickers,
+                             @Nonnull final CandleInterval[] candleIntervals,
+                             final boolean sandboxMode) {
+        this.ssoToken = ssoToken;
+        this.tickers = tickers;
+        this.candleIntervals = candleIntervals;
+        this.sandboxMode = sandboxMode;
+    }
+
+    @Nonnull
+    public static TradingParameters fromProgramArgs(@Nonnull final String ssoTokenArg,
+                                                    @Nonnull final String tickersArg,
+                                                    @Nonnull final String candleIntervalsArg,
+                                                    @Nonnull final String sandboxModeArg) {
+        final String[] tickers = tickersArg.split(",");
+        final CandleInterval[] candleIntervals = Arrays.stream(candleIntervalsArg.split(","))
+                .map(TradingParameters::parseCandleInterval)
+                .toArray(value -> new CandleInterval[0]);
+        if (candleIntervals.length != tickers.length)
+            throw new IllegalArgumentException("Количество переданных разрешающих интервалов свечей не совпадает с переданным количеством тикеров.");
+
+        final boolean useSandbox = Boolean.parseBoolean(sandboxModeArg);
+
+        return new TradingParameters(ssoTokenArg, tickers, candleIntervals, useSandbox);
+    }
+
+    private static CandleInterval parseCandleInterval(final String str) {
+        switch (str) {
+            case "1min":
+                return CandleInterval.ONE_MIN;
+            case "2min":
+                return CandleInterval.TWO_MIN;
+            case "3min":
+                return CandleInterval.THREE_MIN;
+            case "5min":
+                return CandleInterval.FIVE_MIN;
+            case "10min":
+                return CandleInterval.TEN_MIN;
+            default:
+                throw new IllegalArgumentException("Не распознан разрешающий интервал!");
+        }
+    }
+}
