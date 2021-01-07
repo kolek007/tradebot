efe4e0fd4ade0a9c832f232dc082028ad5191d7b
nlyubomishchenko <nlyubomishchenko@axiomsl.com>
2021-01-07 17:25:32 +0300

    Implemented example of subscription on some tickers via specified intervals

---
+++ src/main/java/org/nl/StreamingApiSubscriber.java
@@ -0,0 +1,21 @@
+package org.nl;
+
+import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.example.unicast.AsyncSubscriber;
+import ru.tinkoff.invest.openapi.models.streaming.StreamingEvent;
+
+import javax.annotation.Nonnull;
+import java.util.concurrent.Executor;
+@Slf4j
+public class StreamingApiSubscriber extends AsyncSubscriber<StreamingEvent> {
+    StreamingApiSubscriber(@Nonnull final Executor executor) {
+        super(executor);
+    }
+
+    @Override
+    protected boolean whenNext(final StreamingEvent event) {
+        log.info("Пришло новое событие из Streaming API\n {}", event);
+
+        return true;
+    }
+}
--- src/main/java/org/nl/cfg/TradeBotCfg.java
+++ src/main/java/org/nl/cfg/TradeBotCfg.java
@@ -1,6 +1,7 @@
 package org.nl.cfg;
 
 import org.nl.TickerListener;
+import org.nl.TradingParameters;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,9 +14,22 @@ public class TradeBotCfg {
     @Nonnull
     @Value("${org.nl.ssoToken}")
     private String ssoToken;
+    @Nonnull
+    @Value("#{'${org.nl.tickers}'.split(',')}")
+    private String[] tickers;
+    @Nonnull
+    @Value("#{'${org.nl.candleIntervals}'.split(',')}")
+    private String[] candleIntervals;
+    @Value("${org.nl.sandbox}")
+    private boolean sandbox;
 
     @Bean(initMethod = "init", destroyMethod = "destroy")
     TickerListener getTickerListener() {
-        return new TickerListener(ssoToken);
+        return new TickerListener(getTradingParameters());
+    }
+
+    @Bean
+    TradingParameters getTradingParameters() {
+        return TradingParameters.fromProgramArgs(ssoToken, tickers, candleIntervals, sandbox);
     }
 }
